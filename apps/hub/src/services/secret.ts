60867ef3edb1e1979b3b39d5fd8e50eed9717799
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-01-05 14:35:04 +0100

    Add slate config

--- apps/hub/src/services/secret.ts
+++ apps/hub/src/services/secret.ts
@@ -1,10 +1,33 @@
 import { notFoundError, ServiceError } from '@lowerdeck/error';
 import { Paginator } from '@lowerdeck/pagination';
 import { Service } from '@lowerdeck/service';
-import type { SecretType, Tenant } from '../../prisma/generated/client';
+import type { Secret, SecretType, Tenant } from '../../prisma/generated/client';
 import { db } from '../db';
+import { encryption } from '../encryption';
+import { ID, snowflake } from '../id';
 
 let include = {};
+
+export type SecretSlateInstanceOauthSetup = {
+  input: Record<string, any>;
+  callbackState?: Record<string, any>;
+};
+
+export type SecretSlateOAuthCredentials = {
+  clientId: string;
+  clientSecret: string;
+};
+
+export type SecretSlateAuthConfig = {
+  input?: Record<string, any>;
+  output?: Record<string, any>;
+};
+
+export type SecretTypes = {
+  slate_oauth_setup: SecretSlateInstanceOauthSetup;
+  slate_oauth_credentials: SecretSlateOAuthCredentials;
+  slate_authentication_configuration: SecretSlateAuthConfig;
+};
 
 class secretServiceImpl {
   async getSecretById(d: { id: string; tenant: Tenant }) {
@@ -35,6 +58,80 @@ class secretServiceImpl {
           })
       )
     );
+  }
+
+  async createSecret<Type extends keyof SecretTypes>(d: {
+    tenant: Tenant;
+    purpose: Type;
+    secretData: SecretTypes[Type];
+  }) {
+    let encrypted = await encryption.encrypt({
+      secret: JSON.stringify(d.secretData),
+      entityId: String(d.tenant.oid)
+    });
+
+    return await db.secret.create({
+      data: {
+        oid: snowflake.nextId(),
+        id: await ID.generateId('secret'),
+        type: d.purpose,
+        status: 'active',
+        tenantOid: d.tenant.oid,
+        encryptedSecret: encrypted
+      }
+    });
+  }
+
+  async DANGEROUSLY_decryptSecret<Type extends keyof SecretTypes>(
+    d: ({ secret: Secret } | { secretOid: bigint }) & { purpose: Type; tenant: Tenant }
+  ) {
+    let secret =
+      'secret' in d
+        ? d.secret
+        : await db.secret.findUniqueOrThrow({ where: { oid: d.secretOid } });
+    if (secret.tenantOid !== d.tenant.oid) {
+      throw new Error('WTF - Secret tenant mismatch');
+    }
+
+    if (secret.type !== d.purpose) {
+      throw new Error('WTF - Secret purpose mismatch');
+    }
+
+    let decrypted = await encryption.decrypt({
+      entityId: String(secret.tenantOid),
+      encrypted: secret.encryptedSecret
+    });
+
+    return JSON.parse(decrypted) as SecretTypes[Type];
+  }
+
+  async DANGEROUSLY_updateSecret<Type extends keyof SecretTypes>(
+    d: ({ secret: Secret } | { secretOid: bigint }) & {
+      purpose: Type;
+      tenant: Tenant;
+      secretData: SecretTypes[Type];
+    }
+  ) {
+    let secret =
+      'secret' in d
+        ? d.secret
+        : await db.secret.findUniqueOrThrow({ where: { oid: d.secretOid } });
+    if (secret.type !== d.purpose) {
+      throw new Error('WTF - Secret purpose mismatch');
+    }
+    if (secret.tenantOid !== d.tenant.oid) {
+      throw new Error('WTF - Secret tenant mismatch');
+    }
+
+    let encrypted = await encryption.encrypt({
+      secret: JSON.stringify(d.secretData),
+      entityId: String(secret.tenantOid)
+    });
+
+    return await db.secret.update({
+      where: { oid: secret.oid },
+      data: { encryptedSecret: encrypted }
+    });
   }
 }
 
