0d4c51ef6c8572fb0d0aab9cd0cc445d06bd941a
Ivan <vnxdeveloper@gmail.com>
2026-01-23 12:43:03 +0100

    Fix script

--- apps/hub/init-test-db.sh
+++ apps/hub/init-test-db.sh
@@ -4,7 +4,9 @@ set -e
 # Create databases for CI/local-compose services if they don't exist
 dbs=("slates-hub-test" "slates-registry" "forge" "function-bay" "signal")
 for db in "${dbs[@]}"; do
-  exists=$(psql -tAc "SELECT 1 FROM pg_database WHERE datname='${db}'")
+  exists=$(psql -tAc "SELECT 1 FROM pg_database WHERE datname='${db}'" \
+    --username "$POSTGRES_USER" \
+    --dbname "$POSTGRES_DB")
   if [ "$exists" != "1" ]; then
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" \
       -c "CREATE DATABASE \"${db}\";"
