3afa328cc4e298acc7d1a04d2ce757563c3e5b0c
Ivan <vnxdeveloper@gmail.com>
2026-01-23 12:45:28 +0100

    Update syntax

--- apps/hub/init-test-db.sh
+++ apps/hub/init-test-db.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 # Create databases for CI/local-compose services if they don't exist
-dbs=("slates-hub-test" "slates-registry" "forge" "function-bay" "signal")
-for db in "${dbs[@]}"; do
+dbs="slates-hub-test slates-registry forge function-bay signal"
+for db in $dbs; do
   exists=$(psql -tAc "SELECT 1 FROM pg_database WHERE datname='${db}'" \
     --username "$POSTGRES_USER" \
     --dbname "$POSTGRES_DB")
