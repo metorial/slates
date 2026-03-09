8b84fa652940b61e3c93a7176663b3d27c720fbb
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-03-09 19:43:56 +0100

    Publish

--- apps/hub/src/aresClient.ts
+++ apps/hub/src/aresClient.ts
@@ -3,7 +3,8 @@ import { ProgrammablePromise } from '@lowerdeck/programmable-promise';
 import { createAresInternalClient } from '@metorial-services/ares-client';
 import { env } from './env';
 
-export let aresClient = env.ares.ARES_INTERNAL_URL
+export let aresClient: ReturnType<typeof createAresInternalClient> | null = env.ares
+  .ARES_INTERNAL_URL
   ? createAresInternalClient({
       endpoint: env.ares.ARES_INTERNAL_URL
     })
